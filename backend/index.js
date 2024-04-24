3e78cbad109f6d6d808a356ef39579cf5408c95a
kernelpicnic <worktrojnin@gmail.com>
2024-04-24 22:32:07 +0300

    WIP

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 import Fastify from 'fastify'
+import { fastifyEnv } from '@fastify/env'
 
 const fastify = Fastify({
   logger: true,
+})
+await fastify.register(fastifyEnv, {
+  dotenv: true,
+  schema: {
+    type: 'object',
+    required: ['PORT', 'PRODUCT_ID'],
+    properties: {
+      PORT: {
+        type: 'string',
+        default: 3000,
+      },
+      PRODUCT_ID: {
+        type: 'string',
+        default: '',
+      },
+    },
+  },
 })
 
 // TODO: CORS
@@ -10,7 +28,7 @@ fastify.post('/verify-license-key', async function handler(request, reply) {
   const response = await fetch('https://api.gumroad.com/v2/licenses/verify', {
     method: 'POST',
     body: JSON.stringify({
-      product_id: process.env.PRODUCT_ID,
+      product_id: fastify.config.PRODUCT_ID,
       license_key: request.body.apiKey,
     }),
   })
@@ -18,7 +36,7 @@ fastify.post('/verify-license-key', async function handler(request, reply) {
 })
 
 try {
-  await fastify.listen({ port: process.env.PORT || 3000 })
+  await fastify.listen({ port: fastify.config.PORT })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
