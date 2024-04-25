b283e0d9228463c05a0cc354fb934c8c718a4a93
kernelpicnic <worktrojnin@gmail.com>
2024-04-25 21:41:17 +0300

    WIP

--- backend/index.js
+++ backend/index.js
@@ -8,42 +8,49 @@ const fastify = Fastify({
   logger: true,
 })
 
+async function sendRequest(url, data) {
+  const response = await fetch(`${API_URL}/${url}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+  const json = await response.json()
+  fastify.log.info(json)
+  return json
+}
+
 fastify.post('/verify-license-key', async function handler(request, reply) {
   // TODO: move CORS to global middleware
   reply.header('Access-Control-Allow-Origin', '*')
   reply.header('Access-Control-Allow-Methods', 'POST')
 
-  let status = 'success'
-
   const body = JSON.parse(request.body)
-  const response = await fetch(`${API_URL}/licenses/verify`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      product_id: process.env.PRODUCT_ID,
-      license_key: body.licenseKey,
-      increment_uses_count: false,
-    }),
+  const payload = {
+    product_id: process.env.PRODUCT_ID,
+    license_key: body.licenseKey,
+  }
+
+  const response = await sendRequest('/licenses/verify', {
+    ...payload,
+    increment_uses_count: false,
   })
-  const data = await response.json()
-  if (body.incrementUsesCount && !data.uses) {
+
+  if (!response.success) {
+    return { success: false, status: 'invalid_key' }
+  }
+
+  if (body.incrementUsesCount) {
+    if (response.uses !== 0) {
+      return { success: false, status: 'already_in_use' }
+    }
     // Increment uses count
-    await fetch(`${API_URL}/licenses/verify`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        product_id: process.env.PRODUCT_ID,
-        license_key: body.licenseKey,
-        increment_uses_count: true,
-      }),
+    await sendRequest('/licenses/verify', {
+      ...payload,
+      increment_uses_count: true,
     })
-  } else {
-    status = 'already_in_use'
   }
-  if (!data.success) {
-    status = 'invalid_key'
-  }
-  return { success: data.success, status }
+
+  return { success: true, status: 'success' }
 })
 
 fastify.post('/detach-license-key', async function handler(request, reply) {
@@ -51,17 +58,12 @@ fastify.post('/detach-license-key', async function handler(request, reply) {
   reply.header('Access-Control-Allow-Methods', 'POST')
 
   const body = JSON.parse(request.body)
-  const response = await fetch(`${API_URL}/licenses/decrement_uses_count`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      access_token: process.env.ACCESS_TOKEN,
-      product_id: process.env.PRODUCT_ID,
-      license_key: body.licenseKey,
-    }),
+  const response = await sendRequest('/licenses/decrement_uses_count', {
+    access_token: process.env.ACCESS_TOKEN,
+    product_id: process.env.PRODUCT_ID,
+    license_key: body.licenseKey,
   })
-  const data = await response.json()
-  return { success: data.success, message: data.message }
+  return { success: response.success, message: response.message }
 })
 
 try {
