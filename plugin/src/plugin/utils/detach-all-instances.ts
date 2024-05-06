5ae0b91620752a1710b5210837d2368b9efed1ef
kernelpicnic <worktrojnin@gmail.com>
2024-05-06 22:37:48 +0300

    Fix bad cases

--- plugin/src/plugin/utils/detach-all-instances.ts
+++ plugin/src/plugin/utils/detach-all-instances.ts
@@ -1,12 +1,16 @@
 // Thx to https://github.com/cotlra/figma-detach-selected-instances/blob/main/src/detach-all-instances.ts
 export const detachAllInstances = (nodes: ReadonlyArray<SceneNode>) => {
-  for (let i = 0; i < nodes.length; i++) {
-    const node: SceneNode = nodes[i]
-    if (node.type === 'INSTANCE') {
-      let frame: FrameNode = node.detachInstance()
-      detachAllInstances(frame.children)
-    } else if ('children' in node) {
-      detachAllInstances(node.children)
+  try {
+    for (let i = 0; i < nodes.length; i++) {
+      const node: SceneNode = nodes[i]
+      if (node.type === 'INSTANCE') {
+        let frame: FrameNode = node.detachInstance()
+        detachAllInstances(frame.children)
+      } else if ('children' in node) {
+        detachAllInstances(node.children)
+      }
     }
+  } catch (e) {
+    console.warn('Error trying to detach node - skip it')
   }
 }
