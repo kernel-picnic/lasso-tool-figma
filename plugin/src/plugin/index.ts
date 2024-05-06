be9e6a248548fe270895eb59d7bad672204a7672
kernelpicnic <worktrojnin@gmail.com>
2024-05-06 22:33:49 +0300

    Small improvements

--- plugin/src/plugin/index.ts
+++ plugin/src/plugin/index.ts
@@ -275,35 +275,36 @@ function cutNode(node: SceneNode) {
   if (!node.parent) {
     return []
   }
+  // Working with auto-layout is too complex,
+  // so just disable it; anyway 'CUT' mode
+  // breaks too much to care about it
+  if ('layoutMode' in node && node.layoutMode !== 'NONE') {
+    node.layoutMode = 'NONE'
+  }
   let target = node
-  const isVirtualNode = CONTAINER_NODE_TYPES.includes(node.type)
   // Because we can't cut something from
   // frames, sections and other similar nodes
+  const isVirtualNode = CONTAINER_NODE_TYPES.includes(node.type)
   if (isVirtualNode) {
     node = node as CONTAINER_NODE
     target = figma.createRectangle()
     target.resize(node.width, node.height)
-    target.name = node.name
     node.appendChild(target)
-    if ('fills' in node) {
-      target.fills = node.fills
-      node.fills = []
-    }
+    copyNodeProperties(target, node)
+    // Clear origin styles to prevent styles layering
+    if ('fills' in node) node.fills = []
+    if ('strokes' in node) node.strokes = []
   }
   const parent = node.parent || figma.currentPage
-  const index = parent.children.indexOf(node)
+  const index = isVirtualNode ? 0 : parent.children.indexOf(node)
   const subtract = figma.subtract(
     [copyLasso(), target],
     isVirtualNode ? (node as CONTAINER_NODE) : parent,
-    isVirtualNode ? 0 : index,
+    index,
   )
   copyNodeProperties(subtract, target)
-  // TODO: add option to enable flatten for 'CUT' mode
-  // figma.flatten(
-  //   [subtract],
-  //   parent,
-  //   index,
-  // )
+  // TODO: add option to enable/disable flatten for 'CUT' mode
+  // const flat = figma.flatten([subtract], parent, index)
   return [copyNode(target), subtract]
 }
 
