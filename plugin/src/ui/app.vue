476ff84c69a764c728d491d81385cefadf1c20aa
kernelpicnic <worktrojnin@gmail.com>
2024-05-07 10:58:04 +0300

    Move tooltips to separate component and add tooltip for 'CUT' action

--- plugin/src/ui/app.vue
+++ plugin/src/ui/app.vue
@@ -26,6 +26,10 @@
       <menu-button @click="applyAction(actions.CUT)">
         <div class="icon"><img src="@ui/assets/cut.svg" alt="" /></div>
         Cut
+        <common-tooltip theme="warning">
+          <b>Cut</b> mode will change origin layers. Backup important layers
+          before.
+        </common-tooltip>
       </menu-button>
       <menu-button disabled @click="prettify">
         <div class="icon"><img src="@ui/assets/prettify.svg" alt="" /></div>
@@ -58,13 +62,10 @@
       <menu-button :disabled="!isActiveSelection" @click="applyLasso">
         <div class="icon"><img src="@ui/assets/rectangular.svg" alt="" /></div>
         Use as Lasso
-        <div class="tooltip" v-if="!isActiveSelection">
-          ?
-          <div class="tooltip-content">
-            Choose any <i>vector</i> on&nbsp;the&nbsp;page to&nbsp;use&nbsp;it
-            as&nbsp;a&nbsp;lasso
-          </div>
-        </div>
+        <common-tooltip v-if="!isActiveSelection">
+          Choose any <i>vector</i> on&nbsp;the&nbsp;page to&nbsp;use&nbsp;it
+          as&nbsp;a&nbsp;lasso
+        </common-tooltip>
       </menu-button>
       <!-- TODO -->
       <!--      <app-button :disabled="true" @click="usePrevious">-->
@@ -91,6 +92,7 @@ import MenuButton from '@ui/components/menu-button.vue'
 import LicenseInfo from '@ui/components/license-info.vue'
 import LassoInstruction from '@ui/components/lasso-instruction.vue'
 import HelpInfo from '@ui/components/help-info.vue'
+import CommonTooltip from '@ui/components/common-tooltip.vue'
 
 export default {
   name: 'App',
@@ -99,6 +101,7 @@ export default {
     MenuButton,
     LassoInstruction,
     LicenseInfo,
+    CommonTooltip,
   },
   data() {
     return {
@@ -281,46 +284,6 @@ a {
   a {
     color: var(--figma-color-text-secondary);
   }
-}
-
-.tooltip {
-  width: 20px;
-  height: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--figma-color-bg-brand);
-  border-radius: 50%;
-  margin-left: auto;
-  color: var(--figma-color-text-onbrand);
-  position: relative;
-  line-height: 1;
-  font-size: 10px;
-  font-weight: 500;
-}
-
-.tooltip:hover .tooltip-content {
-  opacity: 1;
-  transform: translateY(0);
-}
-
-.tooltip-content {
-  opacity: 0;
-  transition:
-    opacity 0.2s ease,
-    transform 0.2s ease-in-out;
-  transform: translateY(5px);
-  pointer-events: none;
-  background-color: var(--figma-color-bg-inverse);
-  border-radius: 5px;
-  padding: 8px var(--padding);
-  position: absolute;
-  inset: auto 0 calc(100% + 5px) auto;
-  width: 140px;
-  text-align: left;
-  font-size: 12px;
-  line-height: 1.4;
-  color: var(--figma-color-text-oninverse);
 }
 
 .available-soon {
---
+++ plugin/src/ui/components/common-tooltip.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="['tooltip', theme]">
+    ?
+    <div class="tooltip-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonTooltip',
+  props: {
+    theme: {
+      type: String,
+      default: 'info',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tooltip {
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  margin-left: auto;
+  position: relative;
+  line-height: 1;
+  font-size: 10px;
+  font-weight: 500;
+
+  &.info {
+    background-color: var(--figma-color-bg-brand);
+    color: var(--figma-color-text-onbrand);
+  }
+
+  &.warning {
+    border: 1px solid var(--figma-color-bg-component);
+    color: var(--figma-color-bg-component);
+  }
+}
+
+.tooltip:hover .tooltip-content {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+:global(.tooltip + .arrow) {
+  margin-left: 15px;
+}
+
+.tooltip-content {
+  opacity: 0;
+  transition:
+    opacity 0.2s ease,
+    transform 0.2s ease-in-out;
+  transform: translateY(5px);
+  pointer-events: none;
+  background-color: var(--figma-color-bg-inverse);
+  border-radius: 5px;
+  padding: 8px var(--padding);
+  position: absolute;
+  inset: auto 0 calc(100% + 5px) auto;
+  width: 140px;
+  text-align: left;
+  font-size: 12px;
+  line-height: 1.4;
+  color: var(--figma-color-text-oninverse);
+}
+</style>
